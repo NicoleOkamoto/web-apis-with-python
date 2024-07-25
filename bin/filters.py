f31eb913815a0ec4d221b44e2d4104c301272e31
NicoleOkamoto <nicoleokamoto@icloud.com>
2024-07-25 16:50:45 -0500

    p4 solved

--- bin/filters.py
+++ bin/filters.py
@@ -1,15 +1,12 @@
 from PIL import Image, ImageFilter
 import io
+def apply_filter (file: object, filter:str) -> object:
 
+    image = Image.open(file)
+    image = image.filter(eval( f"ImageFilter. {filter.upper()} " ))
 
-def apply_filter(file: object, filter: str) -> object:
-    """
-    TODO:
-    1. Accept the image as file object, and the filter type as string
-    2. Open the as an PIL Image object
-    3. Apply the filter
-    4. Convert the PIL Image object to file object
-    5. Return the file object
-    """
+    file = io.BytesIO()
+    image.save(file, "JPEG" )
+    file.seek( 0 )
 
-    pass
+    return file
